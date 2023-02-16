d37a7d52920f99041b650054265cc2401384a158
Aliaksandr Konik <cibulyadev@gmail.com>
2023-02-16 04:08:58 +0100

    test

--- src/user/user.service.spec.ts
+++ src/user/user.service.spec.ts
@@ -12,7 +12,7 @@ describe('UserService', () => {
 		service = module.get<UserService>(UserService);
 	});
 
-	it('should be defined', () => {
+	it('should be definedd', () => {
 		expect(service).toBeDefined();
 	});
 });
