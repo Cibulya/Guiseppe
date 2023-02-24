741b3cd7bc0a894ad4633fe1f531e52ffd1eb40b
Aliaksandr Konik <cibulyadev@gmail.com>
2023-02-24 16:29:15 +0100

    feat:rework static files serving

--- src/app.module.ts
+++ src/app.module.ts
@@ -7,7 +7,7 @@ import { UserModule } from './user/user.module';
 @Module({
 	imports: [
 		ServeStaticModule.forRoot({
-			rootPath: path.resolve('./', 'static', 'client'),
+			rootPath: path.resolve('./', 'dist', 'static', 'client'),
 		}),
 		CoffesModule,
 		UserModule,
