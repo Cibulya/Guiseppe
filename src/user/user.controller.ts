c6e6f0ffc9e4b6ee1fca3f2b5f34ecf1573e83f1
Aliaksandr Konik <cibulyadev@gmail.com>
2023-02-28 05:08:10 +0100

    feat:build

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -137,7 +137,7 @@ export class UserController {
 	async activateUser(@Req() req: Request, @Res() res: Response) {
 		try {
 			await this.userService.activate(req);
-			res.status(200).redirect(process.env.DEPLOYED_SERVER);
+			res.status(200).redirect(`${process.env.DEPLOYED_SERVER}activate`);
 		} catch (e) {
 			if (e) {
 				throw new HttpException(
